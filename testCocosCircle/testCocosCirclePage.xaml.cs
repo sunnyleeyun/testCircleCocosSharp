7709cafc6424abafc82709d8b3f7153ec8429b43
Sunny <sunnylee945@gmail.com>
2017-01-15 17:07:39 +0800

    circle move

---
+++ testCocosCircle/testCocosCirclePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace testCocosCircle
+{
+	public partial class testCocosCirclePage : ContentPage
+	{
+		public testCocosCirclePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
